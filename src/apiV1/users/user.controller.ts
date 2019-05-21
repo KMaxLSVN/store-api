7362070b4272b89beb81a7ba2561575c4dc78296
Max <dev@anuitex.com>
2019-05-21 17:48:04 +0300

    user findAll & findOne

--- src/apiV1/users/user.controller.ts
+++ src/apiV1/users/user.controller.ts
@@ -2,53 +2,54 @@ import * as bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import * as jwt from "jwt-then";
 import config from "../../config/config";
-import { User } from "./user.model";
+import { User, users } from "./user.model";
+
 
 export default class UserController {
   public findAll = async (req: Request, res: Response): Promise<any> => {
-    // try {
-    //   const users = await User.find();
-    //   if (!users) {
-    //     return res.status(404).send({
-    //       success: false,
-    //       message: 'Users not found',
-    //       data: null
-    //     });
-    //   }
-    //   res.status(200).send({
-    //     success: true,
-    //     data: users
-    //   });
-    // } catch (err) {
-    //   res.status(500).send({
-    //     success: false,
-    //     message: err.toString(),
-    //     data: null
-    //   });
-    // }
+    try {
+      const result: User[] = await users.findAll();
+      if (!result) {
+        return res.status(404).send({
+          success: false,
+          message: 'Users not found',
+          data: null
+        });
+      }
+      res.status(200).send({
+        success: true,
+        data: result
+      });
+    } catch (err) {
+      res.status(500).send({
+        success: false,
+        message: err.toString(),
+        data: null
+      });
+    }
   };
 
   public findOne = async (req: Request, res: Response): Promise<any> => {
-    // try {
-    //   const user = await User.findById(req.params.id, { password: 0 });
-    //   if (!user) {
-    //     return res.status(404).send({
-    //       success: false,
-    //       message: 'User not found',
-    //       data: null
-    //     });
-    //   }
-    //   res.status(200).send({
-    //     success: true,
-    //     data: user
-    //   });
-    // } catch (err) {
-    //   res.status(500).send({
-    //     success: false,
-    //     message: err.toString(),
-    //     data: null
-    //   });
-    // }
+    try {
+      const user = await users.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).send({
+          success: false,
+          message: 'User not found',
+          data: null
+        });
+      }
+      res.status(200).send({
+        success: true,
+        data: user
+      });
+    } catch (err) {
+      res.status(500).send({
+        success: false,
+        message: err.toString(),
+        data: null
+      });
+    }
   };
 
   public update = async (req: Request, res: Response): Promise<any> => {
